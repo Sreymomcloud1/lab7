27adfa2810ed629c2dcad707cd14638b0a430820
Sreymomcloud1 <sreymomc527@gmail.com>
2026-04-09 22:33:58 +0700

    Delete terraform/variables.tf

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "foodexpress-key" {
-  description = "The name of the AWS key pair"
-  type        = string
-}
-
-variable "image_name" {
-  description = "Docker image name"
-  type        = string
-}
