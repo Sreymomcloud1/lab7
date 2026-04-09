796bf3c93368ac57847f168d4cabc74efd209911
Sreymomcloud1 <sreymomc527@gmail.com>
2026-04-09 14:46:14 +0700

    Refactor AWS resources for FoodExpress application
    Updated security group to allow only HTTP traffic and created a new EC2 instance with updated user data for Docker installation.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,48 +1,17 @@
 provider "aws" {
-  region = "us-east-1" 
+  region = "us-east-1"
 }
 
-resource "aws_instance" "foodexpress_server" {
-  # AMI for Amazon Linux 2023 in us-east-1 (as of early 2026)
-  ami           = "ami-051f8b2110b52e9f0" 
-  instance_type = "t2.micro"
-  key_name      = "foodexpress-key" 
-
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-
-  # This script runs on startup to prepare the environment
-  user_data = <<-EOF
-              #!/bin/bash
-              dnf update -y
-              dnf install -y docker
-              systemctl start docker
-              systemctl enable docker
-              # Give ec2-user permissions to run docker without sudo
-              usermod -aG docker ec2-user
-              EOF
-
-  tags = { 
-    Name = "FoodExpress-Prod-USEast"
-    Project = "FoodExpress"
-  }
-}
-
-resource "aws_security_group" "web_sg" {
+# Security Group
+resource "aws_security_group" "web" {
   name        = "foodexpress-sg"
-  description = "Allow HTTP and SSH"
+  description = "Allow HTTP"
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # In a real job, use your specific IP here!
   }
 
   egress {
@@ -53,6 +22,24 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-output "public_ip" {
-  value = aws_instance.foodexpress_server.public_ip
+# EC2 Instance
+resource "aws_instance" "app" {
+  ami           = "ami-0c02fb55956c7d316" # Ubuntu us-east-1
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.web.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y docker.io
+              systemctl start docker
+              systemctl enable docker
+              docker run -d -p 80:3000 ${var.image_name}
+              EOF
+
+  tags = {
+    Name = "FoodExpress-App"
+  }
 }
