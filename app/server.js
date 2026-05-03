26107b71a6547ce5e149102a22fef04a6ef4f2cf
Sreymomcloud1 <sreymomc527@gmail.com>
2026-05-03 14:07:17 +0700

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Updated from main branch");
+    res.send("Updated from main branch_test1");
 });
 
 app.get('/health', (req, res) => {
