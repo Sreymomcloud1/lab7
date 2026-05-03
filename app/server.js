f44011f28f879bff9c9444c1d1740b4768620163
Sreymomcloud1 <sreymomc527@gmail.com>
2026-05-03 12:16:51 +0700

    main update

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("🚀 DevOps CI/CD Pipeline Running Successfully!");
+    res.send("Updated from main branch");
 });
 
 app.get('/health', (req, res) => {
